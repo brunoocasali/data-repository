0f11e3fd92e4d37c28b230d6de86f36fb39f8295
Bruno Mateus Casali <brunoocasali@gmail.com>
2014-08-11 16:02:35 -0300

    Created from v0.3.1.0

---
+++ BusinessRepository.cs
@@ -0,0 +1,33 @@
+using System.Data;
+
+/// <summary> BusinessRepository v0.3.1.0,
+/// <para></para>
+/// Made by Bruno Casali at Tuesday, June 25, 2014 :/
+/// <!--
+/// You can contact me by: brunoocasali@gmail.com / https://about.me/brunocasali
+/// -->
+/// </summary>
+
+public abstract class BusinessRepository<T>
+{
+    public abstract bool Add();
+    public abstract bool Update();
+    public abstract bool Delete();
+    /// <summary>
+    /// Each class, iplements the call of DataRepository.Total("table_name");
+    /// </summary>
+    /// <returns>How many rows that table has.</returns>
+    public abstract int Total();
+    /// <summary>
+    /// This is a constant to be reused on selects statements
+    /// </summary>
+    protected abstract string Fields { get; }
+    protected abstract T FillAttributes(DataRow dr);
+    public abstract T Select(int id);
+}
+
+public abstract class BusinessRepositoryBase<T> : BusinessRepository<T>
+{
+    protected abstract void FillAttributes(DataRow dr, T obj);
+    public abstract int Total(string search);
+}
