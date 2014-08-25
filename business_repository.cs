cfe3f6768899c8de8396aab384af34783b39fc06
Bruno Mateus Casali <brunoocasali@gmail.com>
2014-08-25 09:30:14 -0300

    Added the namespace to Bussiness DAO

--- business_repository.cs
+++ business_repository.cs
@@ -8,26 +8,29 @@ using System.Data;
 /// -->
 /// </summary>
 
-public abstract class BusinessRepository<T>
+namespace DataRepository
 {
-    public abstract bool Add();
-    public abstract bool Update();
-    public abstract bool Delete();
-    /// <summary>
-    /// Each class, iplements the call of DataRepository.Total("table_name");
-    /// </summary>
-    /// <returns>How many rows that table has.</returns>
-    public abstract int Total();
-    /// <summary>
-    /// This is a constant to be reused on selects statements
-    /// </summary>
-    protected abstract string Fields { get; }
-    protected abstract T FillAttributes(DataRow dr);
-    public abstract T Select(int id);
-}
+    public abstract class BusinessRepository<T>
+    {
+        public abstract bool Add();
+        public abstract bool Update();
+        public abstract bool Delete();
+        /// <summary>
+        /// Each class, iplements the call of DataRepository.Total("table_name");
+        /// </summary>
+        /// <returns>How many rows that table has.</returns>
+        public abstract int Total();
+        /// <summary>
+        /// This is a constant to be reused on selects statements
+        /// </summary>
+        protected abstract string Fields { get; }
+        protected abstract T FillAttributes(DataRow dr);
+        public abstract T Select(int id);
+    }
 
-public abstract class BusinessRepositoryBase<T> : BusinessRepository<T>
-{
-    protected abstract void FillAttributes(DataRow dr, T obj);
-    public abstract int Total(string search);
+    public abstract class BusinessRepositoryBase<T> : BusinessRepository<T>
+    {
+        protected abstract void FillAttributes(DataRow dr, T obj);
+        public abstract int Total(string search);
+    }
 }
